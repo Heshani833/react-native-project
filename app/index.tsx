a171f92237d0ae5dd35d3cb5b8616be1a6de2acb
Heshani833 <heshanishehana123@gmail.com>
2026-02-10 23:30:09 +0530

    chore: initialize project with package.json, TypeScript, and Tailwind CSS configuration
    - Added package.json with dependencies for React Native, Expo, and navigation libraries.
    - Created patches for react-native-web to address deprecation warnings.
    - Configured Tailwind CSS with a new tailwind.config.js file.
    - Added TypeScript configuration with tsconfig.json for strict type checking and path aliases.

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-pink-500">
+      <Text className="text-white text-xl font-bold">
+        Tailwind is working 🚀
+      </Text>
+    </View>
+  );
+}
