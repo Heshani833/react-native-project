ce5dc6b4a1dd8af73992177a4cbbc39d4849725d
Heshani833 <heshanishehana123@gmail.com>
2026-02-11 14:18:13 +0530

    development

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context"; // ← use this one
 import "../global.css";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-pink-500">
-      <Text className="text-white text-xl font-bold">
-        Tailwind is working 🚀
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-pink-500">
+      <View className="flex-1 items-center justify-center">
+        <Text className="text-white text-xl font-bold">
+          Tailwind is working 🚀
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
